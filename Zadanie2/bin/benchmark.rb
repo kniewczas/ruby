a9c6d68abcab97f20de55d8c9c5710b559dbc3a6
kniewczas <kasia-niewczas@wp.pl>
2015-12-01 12:09:47 +0100

    Update benchmark.rb

--- Zadanie2/bin/benchmark.rb
+++ Zadanie2/bin/benchmark.rb
@@ -8,13 +8,13 @@ array_c = (1..5000).map {rand}
 Benchmark.bm(10) do |x|
   x.report('[stack sort] 500 elements:  ') {stackSort(array_a)}
   x.report('[stack sort] 1000 elements: ') {stackSort(array_b)}
-  x.report('[stack sort] 1000 elements: ') {stackSort(array_c)}
+  x.report('[stack sort] 5000 elements: ') {stackSort(array_c)}
 
   x.report('[bubble sort] 500 elements: ') {bubbleSort(array_a)}
   x.report('[bubble sort] 1000 elements:') {bubbleSort(array_b)}
-  x.report('[bubble sort] 1000 elements:') {bubbleSort(array_c)}
+  x.report('[bubble sort] 5000 elements:') {bubbleSort(array_c)}
 
   x.report('[shake sort] 500 elements:  ') {shakeSort(array_a)}
   x.report('[shake sort] 1000 elements: ') {shakeSort(array_b)}
-  x.report('[shake sort] 1000 elements: ') {shakeSort(array_c)}
+  x.report('[shake sort] 5000 elements: ') {shakeSort(array_c)}
   end
