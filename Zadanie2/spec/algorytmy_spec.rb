7a5a76ba66e8e1aa61300ccb9391ed7b35c6ca42
kniewczas <kasia-niewczas@wp.pl>
2015-12-08 10:55:10 +0100

    Update algorytmy_spec.rb

--- Zadanie2/spec/algorytmy_spec.rb
+++ Zadanie2/spec/algorytmy_spec.rb
@@ -1,6 +1,5 @@
 
 require 'simplecov'
-require 'RSpec'
 SimpleCov.start
 
 require_relative '../lib/algorytmy.rb'
@@ -42,25 +41,25 @@ RSpec.describe 'algorytmy.rb' do
 	describe '#bubbleSort' do 
 
 		it 'No error' do
-			expect { stackSort([4,3,25,25,53])}.not_to raise_error
+			expect { bubbleSort([4,3,25,25,53])}.not_to raise_error
 		end
 
 		it 'Sorted list' do
-			expect { stackSort([5,7,81,32,5]).to eq([5,5,7,32,81])}
-			expect { stackSort([5,-7,81,32,-5]).to eq([-5,-7,5,32,81])}
+			expect { bubbleSort([5,7,81,32,5]).to eq([5,5,7,32,81])}
+			expect { bubbleSort([5,-7,81,32,-5]).to eq([-5,-7,5,32,81])}
 		end
 
 		it 'Dealing with the same elements'	do
-			expect { stackSort([4,4,4,4]).to eq([4,4,4,4])}
+			expect { bubbleSort([4,4,4,4]).to eq([4,4,4,4])}
 		end
 
 		it 'Already sorted array' do
-			expect { stackSort([1,2,3,4,5,6,7]).to eq([1,2,3,4,5,6,7])}
+			expect { bubbleSort([1,2,3,4,5,6,7]).to eq([1,2,3,4,5,6,7])}
 		end
 
 		it 'Small array' do
-			expect { stackSort([2,1]).to eq([1,2])}
-			expect { stackSort([3,4,0]).to eq([0,3,4])}
+			expect { bubbleSort([2,1]).to eq([1,2])}
+			expect { bubbleSort([3,4,0]).to eq([0,3,4])}
 		end
 
 	end
@@ -68,36 +67,36 @@ RSpec.describe 'algorytmy.rb' do
 	describe '#shakeSort' do 
 
 		it 'No error' do
-			expect { stackSort([4,3,25,25,53])}.not_to raise_error
+			expect { shakeSort([4,3,25,25,53])}.not_to raise_error
 		end
 
 		it 'Sorted list' do
-			expect { stackSort([5,7,81,32,5]).to eq([5,5,7,32,81])}
-			expect { stackSort([5,-7,81,32,-5]).to eq([-5,-7,5,32,81])}
-			expect { stackSort([4,4,4,4]).to eq([4,4,4,4])}
-			expect { stackSort([1,2,3,4,5,6,7]).to eq([1,2,3,4,5,6,7])}
+			expect { shakeSort([5,7,81,32,5]).to eq([5,5,7,32,81])}
+			expect { shakeSort([5,-7,81,32,-5]).to eq([-5,-7,5,32,81])}
+			expect { shakeSort([4,4,4,4]).to eq([4,4,4,4])}
+			expect { shakeSort([1,2,3,4,5,6,7]).to eq([1,2,3,4,5,6,7])}
 		end
 
 		it 'No error' do
-			expect { stackSort([4,3,25,25,53])}.not_to raise_error
+			expect { shakeSort([4,3,25,25,53])}.not_to raise_error
 		end
 
 		it 'Sorted list' do
-			expect { stackSort([5,7,81,32,5]).to eq([5,5,7,32,81])}
-			expect { stackSort([5,-7,81,32,-5]).to eq([-5,-7,5,32,81])}
+			expect { shakeSort([5,7,81,32,5]).to eq([5,5,7,32,81])}
+			expect { shakeSort([5,-7,81,32,-5]).to eq([-5,-7,5,32,81])}
 		end
 
 		it 'Dealing with the same elements'	do
-			expect { stackSort([4,4,4,4]).to eq([4,4,4,4])}
+			expect { shakeSort([4,4,4,4]).to eq([4,4,4,4])}
 		end
 
 		it 'Already sorted array' do
-			expect { stackSort([1,2,3,4,5,6,7]).to eq([1,2,3,4,5,6,7])}
+			expect { shakeSort([1,2,3,4,5,6,7]).to eq([1,2,3,4,5,6,7])}
 		end
 
 		it 'Small array' do
-			expect { stackSort([2,1]).to eq([1,2])}
-			expect { stackSort([3,4,0]).to eq([0,3,4])}
+			expect { shakeSort([2,1]).to eq([1,2])}
+			expect { shakeSort([3,4,0]).to eq([0,3,4])}
 		end
 	end
 end
